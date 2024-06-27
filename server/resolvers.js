e5ab52ba856d377116316719f879af677d2cb6aa
Jordan Melancon <jordan.melancon@ibm.com>
2024-06-27 10:13:09 -0500

    Section 3 chapter 19

--- server/resolvers.js
+++ server/resolvers.js
@@ -2,5 +2,13 @@ import { getJobs } from './db/jobs.js'
 export const resolvers = {
     Query: {
         jobs: () => getJobs()
+    },
+
+    Job: {
+        date: (job) =>  toISODate(job.createdAt)
     }
+}
+
+const toISODate  = (value) => {
+    return value.slice(0, 'yyyy-mm-dd'.length)
 }
