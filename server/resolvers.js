1eec3ad4accaa89de75e286b623d5c9a46843178
Jordan Melancon <jordan.melancon@ibm.com>
2024-06-27 10:39:44 -0500

    Section 3 chapter 22

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,10 +1,13 @@
 import { getJobs } from './db/jobs.js'
+import { getCompany } from './db/companies.js'
+
 export const resolvers = {
     Query: {
         jobs: () => getJobs()
     },
 
     Job: {
+        company: (job) => getCompany(job.companyId),
         date: (job) =>  toISODate(job.createdAt)
     }
 }
