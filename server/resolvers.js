999118e6ebafe1791aaa2b6efcb7401e82eeb051
Jordan Melancon <jordan.melancon@ibm.com>
2024-06-27 09:45:43 -0500

    Section 3 chapter 17

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,11 +1,18 @@
 export const resolvers = {
     Query: {
-        job: () => {
-            return {
-                id: 'test-id',
-                title: 'The Title',
-                description: 'The description'
-            }
+        jobs: () => {
+            return [
+                {
+                    id: 'test-id1',
+                    title: 'The Title',
+                    description: 'The description'
+                },
+                {
+                    id: 'test-id2',
+                    title: 'The Title',
+                    description: 'The description'
+                }
+            ]
         }
     }
 }
