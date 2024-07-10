bd3231e9d659449ab49ae03e5e67d817417a862f
Jordan Melancon <jordan.melancon@ibm.com>
2024-07-10 11:35:30 -0500

    Section 6 chapter 39

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -2,6 +2,20 @@ import { GraphQLClient, gql } from 'graphql-request';
 
 const client = new GraphQLClient('http://localhost:9000/graphql');
 
+export async function createJob({ title, description}) {
+    const mutation = gql`
+        mutation createJob ($input: CreateJobInput!){
+            job: createJob(input: $input) {
+                id
+            }
+        }
+    `
+    const { job } = await client.request(mutation, {
+        input: { title, description}
+    })
+    return job;
+}
+
 export const getJobs = async() => {
     const query = gql`
     query {
