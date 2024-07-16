8b6f6d1996c04778594b368dc8ae68d424063900
Jordan Melancon <jordan.melancon@ibm.com>
2024-07-16 16:16:34 -0500

    Section 7 chapter 47-48

--- server/resolvers.js
+++ server/resolvers.js
@@ -29,9 +29,26 @@ export const resolvers = {
 
             return createJob({ companyId: user.companyId, title, description})
         },
-        deleteJob: (_root, { id }) => deleteJob(id),
-        updateJob: (_root, {input: {id, title, description}}) => {
-            return updateJob({id, title, description})
+        deleteJob: async (_root, { id }, { user }) => {
+            if (!user) {
+                throw unauthorizedError('Missing authentication')
+            }
+
+            const job = await deleteJob(id, user.companyId)
+            if(!job) {
+                throw notFoundError('No Job found with id ' + id);
+            }
+            return job
+        },
+        updateJob: async (_root, {input: {id, title, description}}, { user }) => {
+            if (!user) {
+                throw unauthorizedError('Missing authentication')
+            }
+            const job = await updateJob({id, companyId: user.companyId, title, description})
+            if(!job) {
+                throw notFoundError('No Job found with id ' + id)
+            }
+            return job
         }
     },
 
